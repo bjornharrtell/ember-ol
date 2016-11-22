1c34518be381e9424f10478ec95259121d948a55
Björn Harrtell <bjorn@wololo.org>
2016-11-22 20:59:49 +0100

    Update test scenarios

--- config/ember-try.js
+++ config/ember-try.js
@@ -2,17 +2,6 @@
 module.exports = {
   scenarios: [
     {
-      name: 'ember-1.13',
-      bower: {
-        dependencies: {
-          'ember': '~1.13.0'
-        },
-        resolutions: {
-          'ember': '~1.13.0'
-        }
-      }
-    },
-    {
       name: 'ember-lts-2.4',
       bower: {
         dependencies: {
@@ -31,28 +20,6 @@ module.exports = {
         },
         resolutions: {
           'ember': 'release'
-        }
-      }
-    },
-    {
-      name: 'ember-beta',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#beta'
-        },
-        resolutions: {
-          'ember': 'beta'
-        }
-      }
-    },
-    {
-      name: 'ember-canary',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#canary'
-        },
-        resolutions: {
-          'ember': 'canary'
         }
       }
     }
