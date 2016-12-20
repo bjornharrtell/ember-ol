5b44965e44d23b0108e76b8a1d4d1de1ce8144ca
Björn Harrtell <bjorn@wololo.org>
2016-12-20 15:51:01 +0100

    Specific node/npm versions

--- deploy.sh
+++ deploy.sh
@@ -46,8 +46,8 @@ SCRIPT_DIR="${BASH_SOURCE[0]%\\*}"
 SCRIPT_DIR="${SCRIPT_DIR%/*}"
 ARTIFACTS=$SCRIPT_DIR/../artifacts
 KUDU_SYNC_CMD=${KUDU_SYNC_CMD//\"}
-NODE_EXE="node"
-NPM_CMD="npm"
+NODE_EXE="$PROGRAMFILES\\nodejs\\6.9.2\\node.exe"
+NPM_CMD="\"$NODE_EXE\" \"$PROGRAMFILES\\npm\\3.10.9\\node_modules\\npm\\bin\\npm-cli.js\""
 NODE_MODULES_DIR="$APPDATA\\npm\\node_modules"
 
 EMBER_PATH="$NODE_MODULES_DIR\\ember-cli\\bin\\ember"
