e0abb1168cf9a1c6d6111ee787cd7d22e6105a08
Björn Harrtell <bjorn@wololo.org>
2017-02-26 20:08:58 +0100

    Add gh-pages deployment

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'dummy',
     environment: environment,
-    rootURL: '/',
+    rootURL: '/ember-ol',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
